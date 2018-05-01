33070292c2bb2c29df9092fb70e03b4f7e501b87
github username <github email address>
2018-05-01 15:09:12 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,15 @@
 class Artist < ActiveRecord::Base
+  has_many :songs
+
+  def song_count
+    self.songs.count
+  end
+
+  # def song_titles
+  #   self.songs.map do |song|
+  #     song.title
+  #   end
+  #
+  # end
+
 end
